2e2c095ddc78aa3b3ab3af84323be9ff22bf84b2
Mark Munter <mark.munter1@gmail.com>
2019-08-22 23:11:06 -0500

    moved out of idea folder

---
+++ functions_1.js
@@ -0,0 +1,39 @@
+// Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+
+function stringTimes(str, n){
+    return str.repeat(n)
+
+}
+
+
+
+// Make a function named isBlank(input) that determines if a given input is spaces, newline characters, or tabs.
+
+
+function isBlank(input){
+    if(input.includes('') || input.includes('\n') || input.includes('\t')){
+        return 'true';
+    }else{
+        return false
+    }
+}
+
+
+
+// Make a function named rollDice(sides) that takes in an argument containing the number of sides the die should have. Generate a random number between 1 up to and - including the number of sides.
+
+
+function rollDice(sides){
+    var dice = Math.floor(Math.random() * sides) + 1;
+    return dice
+}
+rollDice(5)
+
+
+// Write a function called getRandomNumber(min, max) that returns a random number between min and max values sent to that function call.
+
+function getRandomNumber(max, min){
+    var num = Math.floor(Math.random() * max) + min;
+    return num
+}getRandomNumber(5, 1)
